1b9d604ce6332915ac31e6aa8be0f819a7a5c6de
kRaCkrrjAcK <100070459@georgianc.on.ca>
2018-10-13 16:46:55 -0400

    helloworldugly

--- MarcusCOMP2101/helloworldugly.sh
+++ MarcusCOMP2101/helloworldugly.sh
@@ -5,4 +5,4 @@
 
 #This is a silly way creating the output text by starting with something else
 #and stream editing it in a pipeline.
-echo -n "helb wold" |sed -e "s/b/o/g" =e "s/l/ll/" |tr "h" "H"|tr "w" "W"|awk '{prin $1 "\x20" $2 "\41"}'
+echo -n "helb wold" |sed -e "s/b/o/g" -e "s/l/ll/" -e "s/ol/orl/" |tr "h" "H"|tr "w" "W"|awk '{prin $1 "\x20" $2 "\41"}'
