5bacbe55e63aac922eceea43a3c69ef838f076e3
kRaCkrrjAcK <100070459@georgianc.on.ca>
2018-10-13 18:30:10 -0400

    here it comes for myips

---
+++ MarcusCOMP2101/myips.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+#!/bin/sh
+#This is to identify the IP addresses that are used internally & externally by
+#our system.
+#Draw external internet address from $ curl icanhazip.com
+#Draw internal interface with $ ifconfig
+
+ifconfig en enp0s8|grep inet|sed -e 's/.*addr: //' -e 's/.*addr://' -e 's/ .*//'|grep [[:print:]]
+
+
+
+
+i
