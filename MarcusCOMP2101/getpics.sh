26e658c45b6384c8d5d6866cd681633eba68de0f
kRaCkrrjAcK <100070459@georgianc.on.ca>
2018-10-13 19:31:25 -0400

    getpics1

---
+++ MarcusCOMP2101/getpics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash/
+#
+#This script creates a Pictures directory and puts some image files into it,
+#unless they are there already.
+#It makes assumptions that the Pictures directory exists and that it was empty
+#before we started.
+#
+#Assumtion that we have a Pictures directory in our home directory, hardcoded
+#******cd ~/Pictures
+# Assumes you are on-line, hardcoded ******wget -q http://zomzrp.net/pics.icanhazip
+# Sample output:
+#     Found 95 regular files using 7.1M of storage in the ~/Pictures directory
+
+echo -n "Found "find . -type f|wc -l
+echo -n " regular files using "du -sh .|awk '{print $1}'
+echo " of storage in the ~/Pictures directory"
