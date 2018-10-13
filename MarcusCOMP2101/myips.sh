f1c189a7b997bc0709b01bd4403bf7bc8352b337
kRaCkrrjAcK <100070459@georgianc.on.ca>
2018-10-13 18:57:42 -0400

    myips8

--- MarcusCOMP2101/myips.sh
+++ MarcusCOMP2101/myips.sh
@@ -5,8 +5,9 @@
 #Draw external internet address from $ curl icanhazip.com
 #Draw internal interface with $ ifconfig
 
-#ifconfig enp0s8|grep inet|sed -e 's/.*addr: //' -e 's/.*addr://' -e 's/ .*//'|grep [[:print:]]
+#ifconfig enp0s3|grep inet|sed -e 's/.*addr: //' -e 's/.*addr://' -e 's/ .*//'|grep [[:print:]]
 
-echo -n "enp0s3"
-ifconfig enp0s3|grep 'inet '|awk '{print $2}'|sed 's,/.*,,'|echo -n "External IP: "
+echo -n "enp0s3: "
+ifconfig enp0s3|grep 'inet '|awk '{print $2}'|sed 's,/.*,,'
+echo -n "External IP: "
 curl icanhazip.com
