d1b9ae5c0c3a0b9be7b195a97b37ff4518c376c3
kRaCkrrjAcK <100070459@georgianc.on.ca>
2018-10-13 18:53:37 -0400

    myips7

--- MarcusCOMP2101/myips.sh
+++ MarcusCOMP2101/myips.sh
@@ -8,5 +8,5 @@
 #ifconfig enp0s8|grep inet|sed -e 's/.*addr: //' -e 's/.*addr://' -e 's/ .*//'|grep [[:print:]]
 
 echo -n "enp0s3"
-enp0s3|grep 'inet '|awk '{print $2}'|sed 's,/.*,,'|echo -n "External IP: "
+ifconfig enp0s3|grep 'inet '|awk '{print $2}'|sed 's,/.*,,'|echo -n "External IP: "
 curl icanhazip.com
